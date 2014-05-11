e0fa41df4aa140b527b183fbe267de3077a895ea
fieryphoenix <zuev007@mail.ru>
2014-05-11 17:44:57 +0300

    add contours detecting on eyes

--- DEyesTrackerCore/src/by/zuyeu/deyestracker/core/trank/DetectDemo.java
+++ DEyesTrackerCore/src/by/zuyeu/deyestracker/core/trank/DetectDemo.java
@@ -7,8 +7,7 @@ package by.zuyeu.deyestracker.core.trank;
 
 /*
  * Captures the camera stream with OpenCV
- * Search for the faces
- * Display a circle around the faces using Java
+ * Search for the faces and eyes
  */
 import by.zuyeu.deyestracker.core.detection.EyesDetector;
 import by.zuyeu.deyestracker.core.detection.FaceDetector;
@@ -18,6 +17,9 @@ import by.zuyeu.deyestracker.core.video.IFrameCapture;
 import java.awt.Graphics;
 import java.awt.image.BufferedImage;
 import java.awt.image.DataBufferByte;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
@@ -27,9 +29,12 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import org.opencv.core.Core;
 import org.opencv.core.Mat;
+import org.opencv.core.MatOfPoint;
 import org.opencv.core.Point;
 import org.opencv.core.Rect;
 import org.opencv.core.Scalar;
+import org.opencv.core.Size;
+import org.opencv.imgproc.Imgproc;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -96,7 +101,7 @@ public class DetectDemo {
         Rect[] faces = null;
         Rect[] eyes = null;
         final Scalar faceRegionColor = new Scalar(0, 255, 0);
-        final Scalar eyesRegionColor = new Scalar(0, 120, 0);
+        final Scalar eyesRegionColor = new Scalar(120, 120, 120);
 
         ExecutorService executorService = Executors.newSingleThreadExecutor();
         FutureTask<Rect[]> detectFaceTask = null;
@@ -113,12 +118,13 @@ public class DetectDemo {
                     faces = detectFaceTask.get();
                     if (detectEyesTask == null && faces.length > 0) {
                         mainFace = findMainFace(faces);
-                        final Mat faceImage = selectMainFace(mainFace, webcam_image);
+                        final Mat faceImage = selectSubmatByRect(mainFace, webcam_image);
                         Mat faceImageForDetection = preProcessFaceRegion(faceImage);
                         detectEyesTask = createAndRunTask(new DetectEyesTask(eyesDetector, faceImageForDetection), executorService);
                     }
                     if (detectEyesTask != null && detectEyesTask.isDone()) {
                         eyes = detectEyesTask.get();
+                        fixRectFromSubimg(mainFace, eyes);
                     }
                     if (faces.length == 0 || detectEyesTask.isDone()) {
                         detectFaceTask = null;
@@ -128,11 +134,12 @@ public class DetectDemo {
                 frame.setSize(webcam_image.width() + 40, webcam_image.height() + 60);
 
                 if (mainFace != null) {
-                    addRectangleToImage(mainFace, webcam_image, faceRegionColor, null);
+                    addRectangleToImage(mainFace, webcam_image, faceRegionColor);
                 }
-                if (eyes != null) {
-                    final Point shiftPoint = mainFace != null ? new Point(mainFace.x, mainFace.y) : null;
-                    addRectangleToImage(eyes, webcam_image, eyesRegionColor, shiftPoint);
+                if (eyes != null && eyes.length > 0) {
+
+                    addRectangleToImage(eyes, webcam_image, eyesRegionColor);
+                    drawContours(webcam_image, eyes);
                 }
 
                 //-- 4. Display the image
@@ -142,21 +149,60 @@ public class DetectDemo {
         }
     }
 
+    private static void fixRectFromSubimg(Rect mainFace, Rect[] eyes) {
+        final Point shiftPoint = mainFace != null ? mainFace.tl() : new Point(0, 0);
+        Arrays.stream(eyes).forEach(p -> {
+            p.x += shiftPoint.x;
+            p.y += shiftPoint.y;
+        });
+    }
+
+    private static void drawContours(Mat webcam_image, Rect[] eyes) {
+        List<MatOfPoint> contours = new ArrayList<>();
+        final Scalar contourColor = new Scalar(0, 0, 255);
+        for (Rect rect : eyes) {
+            Mat img = selectSubmatByRect(rect, webcam_image);
+            contours.clear();
+            //TODO select onle central contour
+            findContoursOnImg(img, contours);
+            if (contours.size() > 0) {
+                Imgproc.drawContours(img, contours, -1, contourColor);
+                insertSubmatByRect(img, rect, webcam_image);
+            }
+        }
+    }
+
+    private static Rect fixRectPosition(Rect rect, Point shiftPoint) {
+        logger.debug("fixRectPosition() - rect = {}, shift = {}", rect, shiftPoint);
+        rect.x = (int) (rect.x + shiftPoint.x);
+        rect.y = (int) (rect.y + shiftPoint.y);
+        logger.debug("fixRectPosition() - fixed rect = {}", rect);
+        return rect;
+    }
+
+    private static void findContoursOnImg(Mat img, List<MatOfPoint> contours) {
+        Mat imageHSV = new Mat(img.size(), Core.DEPTH_MASK_8U);
+        Mat imageBlurr = new Mat(img.size(), Core.DEPTH_MASK_8U);
+        Mat imageA = new Mat(img.size(), Core.DEPTH_MASK_ALL);
+        Imgproc.cvtColor(img, imageHSV, Imgproc.COLOR_BGR2GRAY);
+        Imgproc.GaussianBlur(imageHSV, imageBlurr, new Size(5, 5), 0);
+        Imgproc.adaptiveThreshold(imageBlurr, imageA, 255, Imgproc.ADAPTIVE_THRESH_MEAN_C, Imgproc.THRESH_BINARY, 7, 5);
+        Imgproc.findContours(imageA, contours, new Mat(), Imgproc.RETR_LIST, Imgproc.CHAIN_APPROX_SIMPLE);
+    }
+
     private static Mat preProcessFaceRegion(final Mat faceImage) {
         //return faceImage.submat(0, faceImage.rows() * 7 / 10, 0, faceImage.cols());
         return faceImage;
     }
 
-    private static void addRectangleToImage(Rect[] regions, Mat webcam_image, final Scalar color, Point shiftPoint) {
+    private static void addRectangleToImage(Rect[] regions, Mat webcam_image, final Scalar color) {
         for (Rect face : regions) {
-            addRectangleToImage(face, webcam_image, color, shiftPoint);
+            addRectangleToImage(face, webcam_image, color);
         }
     }
 
-    private static void addRectangleToImage(Rect face, Mat webcam_image, final Scalar color, Point shiftPoint) {
-        double leftX = face.x + (shiftPoint == null ? 0 : shiftPoint.x);
-        double highY = face.y + (shiftPoint == null ? 0 : shiftPoint.y);
-        Core.rectangle(webcam_image, new Point(leftX, highY), new Point(leftX + face.width, highY + face.height), color);
+    private static void addRectangleToImage(Rect face, Mat webcam_image, final Scalar color) {
+        Core.rectangle(webcam_image, face.tl(), face.br(), color);
     }
 
     private static FutureTask<Rect[]> createAndRunTask(Callable<Rect[]> detectFutureTask, ExecutorService executorService) {
@@ -165,15 +211,14 @@ public class DetectDemo {
         return futureTask;
     }
 
-    private static Mat selectMainFace(Rect mainFace, Mat image) {
-        //TODO: maybe need to change size of face
+    private static Mat selectSubmatByRect(Rect rect, Mat image) {
         double colScale = 1.0 * image.cols() / image.width();
-        int colStart = (int) (1.0 * mainFace.x * colScale);
-        int colEnd = (int) (1.0 * (mainFace.x + mainFace.width) * colScale);
+        int colStart = (int) (1.0 * rect.x * colScale);
+        int colEnd = (int) (1.0 * (rect.x + rect.width) * colScale);
         double rowScale = 1.0 * image.rows() / image.height();
-        int rowStart = (int) (1.0 * mainFace.y * rowScale);
-        int rowEnd = (int) (1.0 * (mainFace.y + mainFace.height) * rowScale);
-        logger.debug("face region: colStart = {}, colEnd = {}, rowStart = {}, rowEnd = {}", colStart, colEnd, rowStart, rowEnd);
+        int rowStart = (int) (1.0 * rect.y * rowScale);
+        int rowEnd = (int) (1.0 * (rect.y + rect.height) * rowScale);
+        logger.trace("region: colStart = {}, colEnd = {}, rowStart = {}, rowEnd = {}", colStart, colEnd, rowStart, rowEnd);
         return image.submat(rowStart, rowEnd, colStart, colEnd);
     }
 
@@ -192,6 +237,21 @@ public class DetectDemo {
             }
         }
         return faces[mainFaceIndex];
+    }
+
+    private static void insertSubmatByRect(Mat subImage, Rect rect, Mat origImage) {
+        double colScale = 1.0 * origImage.cols() / origImage.width();
+        int colStart = (int) (1.0 * rect.x * colScale);
+        int colEnd = (int) (1.0 * (rect.x + rect.width) * colScale);
+        double rowScale = 1.0 * origImage.rows() / origImage.height();
+        int rowStart = (int) (1.0 * rect.y * rowScale);
+        int rowEnd = (int) (1.0 * (rect.y + rect.height) * rowScale);
+        for (int x1 = 0, x2 = colStart; x1 < subImage.cols(); x1++, x2++) {
+            for (int y1 = 0, y2 = rowStart; y1 < subImage.rows(); y1++, y2++) {
+                final double[] subImgData = subImage.get(y1, x1);
+                origImage.put(y2, x2, subImgData);
+            }
+        }
     }
 }
 
---
+++ DEyesTrackerCore/src/by/zuyeu/deyestracker/core/trank/Java8Test.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package by.zuyeu.deyestracker.core.trank;
+
+import java.util.Arrays;
+import org.opencv.core.Point;
+import org.opencv.core.Rect;
+
+/**
+ *
+ * @author Fieryphoenix
+ */
+public class Java8Test {
+
+    public static void main(String[] args) {
+        Rect[] t = new Rect[10];
+        for (int i = 0; i < 10; i++) {
+            t[i] = new Rect(new Point(i, i), new Point(i + 10, i + 10));
+        }
+        printArray(t);
+        Arrays.stream(t).forEach(p -> {
+            p.x += 100;
+            p.y += 200;
+        });
+        printArray(t);
+    }
+
+    private static void printArray(Rect[] t) {
+        Arrays.stream(t).forEach(p -> {
+            System.out.println(" elem = " + p);
+        });
+    }
+}

7e9ebb619494d96677c4f6a922b0af9157c23571
fieryphoenix <zuev007@mail.ru>
2014-05-16 02:26:30 +0300

    renaming - tracker->trackerReader, add structure for teacher ui app

---
+++ DEyesTrackerTeacher/src/by/zuyeu/deyestracker/DEyesTrackerTeacher.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package by.zuyeu.deyestracker;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author Fieryphoenix
+ */
+public class DEyesTrackerTeacher extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("TeachPanel.fxml"));
+
+        final Scene scene = new Scene(root);
+        scene.setOnKeyPressed((KeyEvent t) -> {
+            if (t.getCode() == KeyCode.ESCAPE) {
+                Stage sb = (Stage) scene.getWindow();
+                sb.close();
+            }
+        });
+
+        stage.setScene(scene);
+        stage.setFullScreen(true);
+        stage.setResizable(false);
+        stage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}
